f38d5df21a99b6f6722a4f4a4173d5fba2c1971e
takeshi0518 <y.takeshi0518@gmail.com>
2025-10-24 17:15:53 +0900

    refactor: favicon and mainIcon-color

--- src/components/MainLogo.tsx
+++ src/components/MainLogo.tsx
@@ -4,7 +4,7 @@ import { CloudSun } from 'lucide-react';
 export default function MainLogo() {
   return (
     <Link href="/" className="hover:text-gray-300 transition-colors">
-      <CloudSun className=" w-12 h-12 md:w-14 md:h-14 mb-1 text-yellow-400" />
+      <CloudSun className=" w-12 h-12 md:w-14 md:h-14 mb-1" />
     </Link>
   );
 }
