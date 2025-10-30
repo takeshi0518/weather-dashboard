99b9a7c9270faab069b5fb12ae8923a52e47e194
takeshi0518 <y.takeshi0518@gmail.com>
2025-10-30 18:12:09 +0900

    refactor: DailyWeatherコンポーネントを作成

--- src/components/MainWeatherCard.tsx
+++ src/components/MainWeatherCard.tsx
@@ -1,5 +1,6 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { ArrowRight, CloudSun, Redo } from 'lucide-react';
+import { ArrowRight } from 'lucide-react';
+import DailyWeather from './DailyWeather';
 
 interface MainWeatherCardProps {
   city: string | undefined;
@@ -22,41 +23,20 @@ export default function MainWeathrCard({
 
       <CardContent className="p-4 md:p-6">
         <div className="flex justify-around">
-          <div className="text-center space-y-2">
-            <h2 className="text-sm sm:text-lg md:text-2xl font-bold text-gray-900">
-              今日
-            </h2>
-            <div className="flex flex-col md:flex-row items-center gap-3 mt-4">
-              <CloudSun className="w-12 md:w-14 h-12 md:h-14 text-blue-500" />
-              <div className="md:ml-3">
-                <div className="text-3xl md:text-4xl font-bold text-gray-900">
-                  {temp}℃
-                </div>
-                <p className="text-sm md:text-base text-gray-600">
-                  {description}
-                </p>
-              </div>
-            </div>
-          </div>
-
+          <DailyWeather
+            label="今日"
+            temp={temp}
+            description={description}
+            iconColor="text-blue-500"
+          />
           <ArrowRight className="w-8 h-8 md:w-12 md:h-12 text-gray-400 hidden md:block" />
 
-          <div className="text-center space-y-2">
-            <h2 className="text-sm sm:text-lg md:text-2xl font-bold text-gray-900">
-              今日
-            </h2>
-            <div className="flex flex-col md:flex-row items-center gap-3 mt-4">
-              <CloudSun className="w-12 md:w-14 h-12 md:h-14 text-blue-500" />
-              <div className="md:ml-3">
-                <div className="text-3xl md:text-4xl font-bold text-gray-900">
-                  {temp}℃
-                </div>
-                <p className="text-sm md:text-base text-gray-600">
-                  {description}
-                </p>
-              </div>
-            </div>
-          </div>
+          <DailyWeather
+            label="明日"
+            temp={temp}
+            description={description}
+            iconColor="text-blue-500"
+          />
         </div>
       </CardContent>
     </Card>
